ea1baa0da4c3e640043d5fc5843c4850769641d4
Font2099 <jpfont18@gmail.com>
2025-05-22 17:39:11 -0300

    Add files via upload

---
+++ Atividade1.py
@@ -0,0 +1,46 @@
+
+#Comentário
+#dados primitivo
+#para caareegar o código, clique f5
+#strings = texto
+
+print("Bem vindo a calculadora")
+print()
+
+#SOMA
+print("Soma:")
+print()
+n1 = int(input("Escolha um número"))
+n2 = int(input("Escolha outro número"))
+soma = n1 + n2
+print(soma)
+print()
+
+
+#SUBTRAÇÃO
+print("Subtração:")
+print()
+n3 = int(input("Escolha um número"))
+n4 = int(input("Escolha outro número"))
+subtração = n3 - n4
+print(subtração)
+print()
+
+
+#MULTIPLICAÇÃO
+print("Multiplicação:")
+print()
+n5 = int(input("Escolha um número"))
+n6 = int(input("Escolha outro número"))
+multiplicação = n5 * n6
+print(multiplicação)
+print()
+
+#DIVISÃO
+print("Divisão:")
+print()
+n7 = int(input("Escolha um número"))
+n8 = int(input("Escolha outro número"))
+divisão = n7 / n8
+print(divisão)
+
