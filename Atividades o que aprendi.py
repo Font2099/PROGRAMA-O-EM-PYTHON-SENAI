45e3b0862541685f45d0c59afc9c60c87c14f68b
Font2099 <jpfont18@gmail.com>
2025-05-23 17:51:00 -0300

    Add files via upload

---
+++ Atividades o que aprendi.py
@@ -0,0 +1,44 @@
+#ATIVIDAE 1 
+print('SEJA BEM VINDO')
+
+#ATIVIDADE 2
+verdadeiro = True
+
+print(type(verdadeiro))
+
+#ATIVIDADE 3
+print(10.5 * 2.00)
+
+#ATIVIDADE 4
+print(10 / 2)
+
+#ATIVIDADE 5
+print(10 - 5)
+
+#ATIVIDADE 6
+print(10 //  2)
+
+#ATIVIDADE 7
+print(2.0 * 3.0 * 10.0 * 7.0)
+
+#ATIVIDADE 8
+numero = 5
+print(numero **2)
+
+#ATIVIDADE 9
+ 
+nome = input('Digite seu nome:')
+idade = int(input("Digite dua idade:"))
+email = input('Digite seu email: ' )
+endereco = input('Digite seu endereço:')
+curso = input('Digite seu curso')
+
+print(f'''
+
+nome: {nome}
+idade: {idade}
+e-mail: {email}
+endereço: {endereco}
+curso: {curso}
+
+''')
