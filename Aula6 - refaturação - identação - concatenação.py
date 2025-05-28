6446bc94f1fa9e1659fa3d7ea065dadde3d82aed
Font2099 <jpfont18@gmail.com>
2025-05-28 16:48:16 -0300

    Add files via upload

---
+++ Aula6 - refaturação - identação - concatenação.py
@@ -0,0 +1,10 @@
+#indentação em python - organizar o código py
+# tipo de organização do código
+
+
+nome = 'Julio'
+sobrenome = 'Cezar'
+
+print('hello', nome, sobrenome )
+
+c = 10
---
+++ exercicios.aula6.py
@@ -0,0 +1,90 @@
+resultado = (4*6) / 2
+print('resultado = ', resultado)
+
+#Atividade 2
+soma = 5 + 7
+print('resultado = ', soma)
+
+#ATIVIDADE 3
+elevado = 3**4
+print('resultado = ', elevado)
+
+#ATIVIDADE 4
+media = (8 + 12 + 15)/ 3
+print('resultado = ', media)
+
+#ATIVIDADE 5
+subtracao = (2 - 10)*5
+print('resultado = ', subtracao)
+
+#ATIVIDADE 6
+divisao = (27/3)+5
+print('resultado = ', divisao)
+
+#ATIVIDAE 7
+modulo = 17 % 4
+print('Resultado = ', modulo)
+
+#ATIVIDADE 8
+elevado2 = (9*3)**2
+print('resultado = ', elevado2)
+
+#ATIVIDADE 9
+raiz = 81**0,5
+print('resultado = ', raiz)
+
+
+#ATIVIDADE 10
+add = 4* 20 + 3
+print('Resultado = ', add)
+
+#ATIVIDADE 11
+sub = 15 * 2 - 7
+print('Resultado = ', sub)
+
+#ATIVIDADE 12
+elev = 5**3
+print('Resultado = ', elev)
+
+#ATIVIDADE 13
+med = (17 + 21 + 25)/3
+print('Resultado = ', med)
+
+#ATIVIDADE 14
+mult = 11 * 2 + 7
+print('Resultado = ', mult)
+
+#ATIVIDADE 15
+div = (15 - (3*8))/2
+print('Resultado = ', div)
+
+
+#ATIVIDADE 16
+eleve2 = 2**10
+print('Resultado = ', eleve2)
+
+
+palavra = 'ISSO É UM TESTE EM PYTHON'
+teste = 'ushhisbfisbfwyfw uydtwgdtwdw tdytafyid wi diugfdgysvdwf edf '
+
+comprimento = len(palavra)
+
+
+
+print(comprimento)
+
+print(len(teste))
+
+palavra = 'Isso é um teste'
+
+lista = list(palavra)
+print(lista)
+
+tupla = ('tr', 'ana','Pedro')
+print(tupla)
+
+#range()
+
+lista = list(range(0,201,10))
+
+print(lista)
