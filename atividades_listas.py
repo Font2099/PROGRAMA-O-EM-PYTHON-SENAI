cbb526ee3170f8c164ed983e404cb405fc5ec341
Font2099 <jpfont18@gmail.com>
2025-05-29 15:55:43 -0300

    Add files via upload

---
+++ atividades_listas.py
@@ -0,0 +1,23 @@
+# EXERCÍCIO 0
+numeros = list(range(0,21,2))
+print(numeros)
+
+# EXERCÍCIO 1
+numeros = [0,1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+# EXERCÍCIO 2
+print(numeros[3])
+
+# EXERCÍCIO 3
+numeros.append(9)
+print(numeros)
+
+# EXERCÍCIO 4
+del numeros[5]
+print(numeros)
+
+# EXERCÍCIO 5
+carros = ['Civic', 'Fusca',' Uno']
+carros +=(numeros)
+print(carros)
